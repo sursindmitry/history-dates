11bb3001569254a9704990865ad809b3a4b84ec5
Dmitry Sursin <sursindmityi@gmail.com>
2023-04-04 21:12:00 +0700

    Cards

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ function App() {
         <div className="mainName">
           <h1>Исторические даты</h1>
         </div>
+          <div className="cards">
+              <div className="card">
+                  <b>2015</b>
+                  <h4>13 сентября — частное солнечное затмение, видимое в Южной Африке и части Антарктиды</h4>
+              </div>
+
+              <div className="card">
+                  <b>2016</b>
+                  <h4>Телескоп «Хаббл» обнаружил самую удалённую из всех обнаруженных галактик, получившую обозначение GN-z11</h4>
+              </div>
+
+              <div className="card">
+                  <b>2017</b>
+                  <h4>Компания Tesla официально представила первый в мире электрический грузовик Tesla Semi</h4>
+              </div>
+
+              <div className="btnList">
+                  <button><i className="arrowRight"></i></button>
+              </div>
+          </div>
       </div>
     </div>
   );
